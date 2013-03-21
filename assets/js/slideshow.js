20fb62dbab9539769e080beaed4aff1de78440c0
Ellen Gummesson <ellengummesson@gmail.com>
2013-03-21 16:47:55 +0100

    Added 'slideshow'.

---
+++ assets/js/slideshow.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+  var slideSpeed, fadeSpeed;
+
+  slideSpeed = 4000;
+  fadeSpeed  = 2000;
+
+  $(".slideshow-item:gt(0)").hide();
+  
+  setInterval(function(){
+    $(".slideshow-item:first-child").fadeOut(fadeSpeed)
+      .next(".slideshow-item").fadeIn(fadeSpeed)
+      .end().appendTo(".slideshow");
+    }, slideSpeed);
+
+});
