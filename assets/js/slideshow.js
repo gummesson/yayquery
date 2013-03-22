5455211fa48a60f21111aeaee8c3924c5e624de9
Ellen Gummesson <ellengummesson@gmail.com>
2013-03-22 20:07:02 +0100

    Changed structure.

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -8,11 +8,13 @@ $(document).ready(function() {
   fadeSpeed  = 2000;
 
   $(".slideshow-item:gt(0)").hide();
-  
-  setInterval(function(){
+
+  function slideShow() {
     $(".slideshow-item:first-child").fadeOut(fadeSpeed)
-      .next(".slideshow-item").fadeIn(fadeSpeed)
-      .end().appendTo(".slideshow");
-    }, slideSpeed);
+    .next(".slideshow-item").fadeIn(fadeSpeed)
+    .end().appendTo(".slideshow");
+  }
+
+  setInterval(slideShow, slideSpeed);
 
 });
